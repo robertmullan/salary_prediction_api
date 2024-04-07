126bcea90ffa964393dddc075aee45cd846156cb
mikecolbert <michael-colbert@uiowa.edu>
2024-04-07 12:19:25 -0500

    functional starting app.py
    app.py contains basic routes to test Flask functionality

---
+++ app.py
@@ -0,0 +1,18 @@
+import flask
+from flask import request
+from flask_cors import CORS
+
+app = flask.Flask(__name__)
+
+CORS(app)
+
+# main index page (root route)
+@app.route("/")
+def home():
+    return "<h1>Salary Prediction API</h1><p>BAIS:3300 - Digital Product Development</p><p>Mike Colbert</p>"
+
+
+# predict route
+@app.route("/predict", methods=["GET"])
+def predict():
+    return "<h1>Prediction route is working... </h1>"
