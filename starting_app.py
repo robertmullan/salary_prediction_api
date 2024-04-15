5635d8f4dff556692a9ef6ee595b873761ff2441
Robert Mullan <rmullan@uiowa.edu>
2024-04-15 14:01:38 -0500

    Clean-up

--- starting_app.py
+++ starting_app.py
@@ -9,7 +9,7 @@ CORS(app)
 # main index page (root route)
 @app.route("/")
 def home():
-    return "<h1>Salary Prediction API</h1><p>BAIS:3300 - Digital Product Development</p><p>Mike Colbert</p>"
+    return "<h1>Salary Prediction API</h1><p>BAIS:3300 - Digital Product Development</p><p>Robert Mullan</p>"
 
 
 # predict route
